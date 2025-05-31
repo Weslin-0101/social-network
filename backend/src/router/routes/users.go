0f17ad75a07e70cd18683d9bbefa221d7dc43635
Weslin-0101 <wesleeylirac12@gmail.com>
2025-05-31 14:37:07 -0300

    feat: creating the routes struct and the users routes

---
+++ backend/src/router/routes/users.go
@@ -0,0 +1,38 @@
+package routes
+
+import (
+	"backend/src/controllers"
+)
+
+var userRoutes = []Route {
+	{
+		URI: "/users",
+		Method: "POST",
+		Function: controllers.CreateUser,
+		AuthRequired: false,
+	},
+	{
+		URI: "/users",
+		Method: "GET",
+		Function: controllers.GetAllUsers,
+		AuthRequired: false,
+	},
+	{
+		URI: "/users/{userID}",
+		Method: "GET",
+		Function: controllers.GetUserByID,
+		AuthRequired: false,
+	},
+	{
+		URI: "/users/{userID}",
+		Method: "PUT",
+		Function: controllers.UpdateUserByID,
+		AuthRequired: false,
+	},
+	{
+		URI: "/users/{userID}",
+		Method: "DELETE",
+		Function: controllers.DeleteUserByID,
+		AuthRequired: false,
+	},
+}
