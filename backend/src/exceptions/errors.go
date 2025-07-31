6382b2980e7fe4fc032dfd749cf34aba62442e7b
Weslin-0101 <wesleeylirac12@gmail.com>
2025-07-30 21:40:09 -0300

    feat: adding more messages into the exception message handler

--- backend/src/exceptions/errors.go
+++ backend/src/exceptions/errors.go
@@ -5,4 +5,8 @@ import "errors"
 var (
 	ErrUserNotFound = errors.New("user not found")
 	ErrInvalidUserID = errors.New("invalid user ID")
+	ErrInvalidUserNickname = errors.New("invalid user nickname")
+	ErrDatabaseConnection = errors.New("failed to connect to the database")
+	ErrInternalServer = errors.New("internal server error occurred")
+	ErrBadRequest = errors.New("invalid request body format")
 )
--- backend/src/exceptions/handle_exceptions.go
+++ backend/src/exceptions/handle_exceptions.go
@@ -1,10 +1,25 @@
 package exceptions
 
 import (
+	"backend/src/model"
 	"encoding/json"
 	"log"
 	"net/http"
 )
+
+type ErrorResponse struct {
+	URL 	string 	`json:"url"`
+	Type 	int 	`json:"type"`
+	Message string 	`json:"message"`
+}
+
+type ValidationErrorResponse struct {
+	URL     string `json:"url"`
+	Type    int    `json:"type"`
+	Message string `json:"message"`
+	Field   string `json:"field"`
+	Code    string `json:"code"`
+}
 
 func HandleResponse(w http.ResponseWriter, statusCode int, data interface{}) {
 	w.WriteHeader(statusCode)
@@ -14,10 +29,44 @@ func HandleResponse(w http.ResponseWriter, statusCode int, data interface{}) {
 	}
 }
 
-func HandleError(w http.ResponseWriter, statusCode int, err error) {
-	HandleResponse(w, statusCode, struct {
-		Erro string `json:"error"`
-	}{
-		Erro: err.Error(),
-	})
+func HandleError(
+	w http.ResponseWriter,
+	r *http.Request,
+	statusCode int,
+	err error,
+) {
+	if validationErr, ok := err.(model.ValidationError); ok {
+		errorResponse := ValidationErrorResponse {
+			URL:	 r.URL.Path,
+			Type: 	 statusCode,
+			Message: validationErr.Message,
+			Field:   validationErr.Field,
+			Code:    validationErr.Code,
+		}
+		HandleResponse(w, statusCode, errorResponse)
+		return
+	}
+
+	errorResponse := ErrorResponse {
+		URL:	 r.URL.Path,
+		Type: 	 statusCode,
+		Message: err.Error(),
+	}
+
+	HandleResponse(w, statusCode, errorResponse)
+}
+
+func HandleErrorWithCustomMessage(
+	w http.ResponseWriter,
+	r *http.Request,
+	statuCode int,
+	message string,
+) {
+	errorResponse := ErrorResponse {
+		URL:	 r.URL.Path,
+		Type: 	 statuCode,
+		Message: message,
+	}
+
+	HandleResponse(w, statuCode, errorResponse)
 }
